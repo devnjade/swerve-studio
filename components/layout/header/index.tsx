0db4402bbff145f5eb1e72237ea1b66dfd8365ad
Adebayo Temitope <adecancode@gmail.com>
2022-07-23 02:07:31 +0100

    update

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './index.module.scss';
 
 const Header: React.FC = () => {
   return (
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,9 +1,11 @@
+import { Loader } from "components/layout";
 import React from "react";
+import styles from './index.module.scss';
 
 const HomeView: React.FC = () => {
   return (
     <div>
-
+      <Loader />
     </div>
   )
 }
